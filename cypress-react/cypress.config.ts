83eef5e8699ab754f1598d2a5f7c4783cb7fb2df
DanishSjjd <danishsjjad@gmail.com>
2024-02-14 11:28:18 +0500

    feat(cypress-react): cypress code coverage

--- cypress-react/cypress.config.ts
+++ cypress-react/cypress.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "cypress"
+import codeCoverage from "@cypress/code-coverage/task"
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: "cypress/**/*.*",
+    },
+  },
   e2e: {
     baseUrl: "http://localhost:3001",
     setupNodeEvents(on, config) {
-      //
+      codeCoverage(on, config)
+      return config
     },
   },
 })
--- cypress-react/vite.config.mts
+++ cypress-react/vite.config.mts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import istanbul from "vite-plugin-istanbul"
 
 export default defineConfig({
-  plugins: [react()],
-  server: {},
+  plugins: [
+    react(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+  ],
 })
