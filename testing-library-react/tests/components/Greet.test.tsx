9af8bf22b6afa664134687aa55c72a97516d38b9
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-04-27 06:02:05 +0500

    feat: greet component tests

--- testing-library-react/tests/components/Greet.test.tsx
+++ testing-library-react/tests/components/Greet.test.tsx
@@ -1,11 +1,22 @@
-import { render, screen } from "@testing-library/react"
-import { describe, it } from "vitest"
+import { it, expect, describe } from "vitest"
 import Greet from "../../src/components/Greet"
+import { render, screen } from "@testing-library/react"
 
 describe("Greet", () => {
   it("should return name with Hello when name is provided", () => {
     render(<Greet name="Danish" />)
 
+    const heading = screen.getByRole("heading")
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveTextContent(/danish/i)
+  })
+
+  it("should return login button when name is not provided", () => {
+    render(<Greet name="" />)
+
+    const button = screen.getByRole("button")
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent(/login/i)
     screen.debug()
   })
 })
